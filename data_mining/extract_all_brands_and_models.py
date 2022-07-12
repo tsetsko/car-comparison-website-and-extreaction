817c208cd076d6a9305d60a30be7495a69db3456
tsetsko <t.donov94@gmail.com>
2022-07-12 23:33:06 +0300

    continue work on scraper

--- data_mining/extract_all_brands_and_models.py
+++ data_mining/extract_all_brands_and_models.py
@@ -1,32 +1,74 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
-from datetime import datetime
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+import pandas as pd
 
 URL = "https://www.mobile.bg/pcgi/mobile.cgi"
 
-# PATH = '/Applications/chromedriver' for mac
-PATH = "C:\Windows\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
+PATH = '/Applications/chromedriver'
+# PATH = "/Applications/geckodriver"
+BRANDS_TO_LOOK_FOR = ["Audi", "BMW", "Mercedes-Benz", "Skoda", "VW"]
+# PATH = "C:\Windows\chromedriver.exe"
+driver = webdriver.Chrome(executable_path=PATH)
 driver.get(URL)
 page_source = driver.page_source
 soup = BeautifulSoup(page_source, "html.parser")
 
-all_car_brands = [brand.text.replace("\n", "") for brand in soup.find("select", {"name": "marka"}).findChildren("option", recursive=False)]
-
+all_car_brands = [brand.text.replace("\n", "") for brand in soup.find("select", {"name": "marka"}).findChildren("option", recursive=False) if brand.text != "всички\n"]
+print(all_car_brands)
 
 models = []
-for brand in all_car_brands[5:20]:
-    select = Select(driver.find_element_by_name("marka"))
-    select.select_by_visible_text(brand)
-    new_page_source = driver.page_source
-    new_soup = BeautifulSoup(new_page_source, "html.parser")
-    time.sleep(1)
-    models.append({brand: [model.text for model in new_soup.find("select", {"name": "model"}).findChildren("option", recursive=False)]})
-    
+for brand in all_car_brands:
+    if brand in BRANDS_TO_LOOK_FOR:
+        # models.append({brand: []})
+        select = Select(driver.find_element(By.NAME, "marka"))
+        select.select_by_visible_text(brand)
+        new_page_source = driver.page_source
+        new_soup = BeautifulSoup(new_page_source, "html.parser")
+        model_to_loop = [model.text for model in new_soup.find("select", {"name": "model"}).findChildren("option", recursive=False) if model.text != "всички\n"]
+        print(model_to_loop)
+        for model in model_to_loop:
+            models.append({f"{brand}-{model}": []})
+            # I do this to get rid of the initial popup
+            try:
+                driver.find_element(By.XPATH, "/html/body/div[5]/div[2]/div[1]/div[2]/div[2]/button[1]").click()
+                driver.implicitly_wait(1)
+            except:
+                pass
+            # First always select the "всички" category so that the models for the different brands can be refreshed.
+            driver.find_element(By.NAME, "marka").click()
+            driver.implicitly_wait(2)
+            general_select = Select(driver.find_element(By.NAME, "marka"))
+            driver.implicitly_wait(2)
+            general_select.select_by_visible_text("всички")
+            # This is the selector for the brand
+            driver.find_element(By.NAME, "marka").click()
+            time.sleep(2)
+            brand_select = Select(driver.find_element(By.NAME, "marka"))
+            brand_select.select_by_visible_text(brand)
+            driver.implicitly_wait(2)
+            # This is the selector for the model
+            driver.find_element(By.NAME, "model").click()
+            driver.implicitly_wait(2)
+            model_select = Select(driver.find_element(By.NAME, "model"))
+            model_select.select_by_value(model)
+            driver.implicitly_wait(2)
+            driver.find_element(By.ID, "button2").click()
+            driver.implicitly_wait(2)
+            current_url = driver.current_url
+            where_to_look_for = brand + "-" + model
+            models.append({where_to_look_for: current_url})
+            # for i in models:
+            #     for key, value in i.items():
+            #         if key == brand:
+            #             value.append({model: current_url})
+            driver.implicitly_wait(2)
+            driver.execute_script("window.history.go(-1)")
+            time.sleep(2)
+    df = pd.DataFrame.from_dict(models)
+    print(df)
+    df.to_csv(f"car_brands_and_models_links.csv")
 
-print(all_car_brands)
-print(len(all_car_brands))
 print(models)
-print(len(models))
--- data_mining/selenium_miner.py
+++ data_mining/selenium_miner.py
@@ -1,29 +1,38 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
 import time
 from datetime import datetime
-import pandas as pd
+import random
+from sys import platform
 
-URL = "https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=oxiy89&f1"
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+
+URL = "https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=oxonz5&f1"
 EUR_TO_LEV = 1.95583
 TODAYS_DATE = datetime.today().strftime('%Y-%m-%d')
 month_to_number = {"януари": 1, "февруари": 2, "март": 3, "април": 4, "май": 5, "юни": 6, "юли": 7, "август": 8, "септември": 9, "октовмри": 10, "ноември": 11, "декември": 12}
 
 
 def get_page_source_with_selenium(url_link):
-    # PATH = '/Applications/chromedriver' for mac
-    PATH = "C:\Windows\chromedriver.exe"
-    driver = webdriver.Chrome(PATH)
+    path = ""
+    if platform == "darwin":
+        path = '/Applications/chromedriver'
+    elif platform == "win32":
+        path = "C:\Windows\chromedriver.exe"
+    driver = webdriver.Chrome(path)
     driver.get(url_link)
     page_source = driver.page_source
     soup = BeautifulSoup(page_source, "html.parser")
     return soup
+
 
 def get_last_page(html_data):
     all_pages = html_data.find("span", {"class": "pageNumbersInfo"})
     # Extract only the last number
     last_page = all_pages.text.split(" ")[-1]
     return int(last_page)
+
 
 last_page = get_last_page(get_page_source_with_selenium(URL))
 
@@ -33,8 +42,9 @@ def list_of_all_pages(last_page_for_generation):
     list_of_urls = [URL + f"={page}" for page in range(1, last_page + 1)]
     return list_of_urls
 
+
 pages_with_car_offers = list_of_all_pages(last_page)
-print(pages_with_car_offers)
+
 
 # Once we have all the pages one must gather all links for the actual cars.
 def get_all_links_of_cars(html_parsed, list_to_append):
@@ -45,43 +55,56 @@ def get_all_links_of_cars(html_parsed, list_to_append):
 
 
 def get_exact_data_for_each_car(html_link):
-    # This gets the general stats of for each car
     car_dict = {}
     try:
         general_data = html_link.find('ul', {'class': 'dilarData'}).findChildren("li", recursive=False)
-        general_data_only_text = [info.text for count, info in enumerate(general_data) if count % 2 and count != 0]
-        if len(general_data_only_text) == 8:
-            car_dict["Date_of_extraction"] = TODAYS_DATE
-            car_dict["Month_of_production"] = month_to_number.get(general_data_only_text[0].split(" ")[0])
-            car_dict["Year_of_production"] = general_data_only_text[0].split(" ")[1].replace("г.", "")
-            car_dict["Type_of_engine"] = general_data_only_text[1]
-            car_dict["Horsepower"] = general_data_only_text[2]
-            car_dict["Eurostandard"] = general_data_only_text[3]
-            car_dict["Transmission"] = general_data_only_text[4]
-            car_dict["Car_type"] = general_data_only_text[5]
-            car_dict["Kilometers_traveled"] = general_data_only_text[6].split(" ")[0]
-            car_dict["Colour"] = general_data_only_text[7]
-            # This gets the price of the car
-            detailed_price_general = html_link.find('span', {'id': 'details_price'}).text
-            detailed_price = 0
-            if "лв." in detailed_price_general:
-                detailed_price = int(detailed_price_general.replace('лв.', '').replace(" ", ""))
-            else:
-                detailed_price = round(int(detailed_price_general.replace('EUR', '').replace(" ", "")) * EUR_TO_LEV, 0)
-            # price_dict = {"Price": detailed_price}
-            car_dict["Price"] = detailed_price
-            # Get all additional features
-            additional_features = html_link.find_all('label', {'class': 'extra_cat'})
-            specific_element = []
-            for element in additional_features:
-                for element1 in element.find_next_siblings('div'):
-                    specific_element.append(element1.text.replace('•', '').strip())
-            # additinal_elements_dict = {"Additional Data": specific_element}
-            car_dict["Additional_Data"] = specific_element
+        general_data_only_text = [info.text for info in general_data]
+        car_dict["Date_of_extraction"] = TODAYS_DATE
+        if "Дата на производство" in general_data_only_text:
+            month_of_production_index = general_data_only_text.index("Дата на производство") + 1
+            car_dict["Month_of_production"] = int(month_to_number.get(general_data_only_text[month_of_production_index].split(" ")[0]))
+            car_dict["Year_of_production"] = int(general_data_only_text[month_of_production_index].split(" ")[1].replace("г.", ""))
+        if "Тип двигател" in general_data_only_text:
+            type_of_engine_index = general_data_only_text.index("Тип двигател") + 1
+            car_dict["Type_of_engine"] = general_data_only_text[type_of_engine_index]
+        if "Мощност" in general_data_only_text:
+            horsepower = general_data_only_text.index("Мощност") + 1
+            car_dict["Horsepower"] = float(general_data_only_text[horsepower].split(" ")[0])
+        if "Евростандарт" in general_data_only_text:
+            eurostandard = general_data_only_text.index("Евростандарт") + 1
+            car_dict["Eurostandard"] = general_data_only_text[eurostandard]
+        if "Скоростна кутия" in general_data_only_text:
+            transmission = general_data_only_text.index("Скоростна кутия") + 1
+            car_dict["Transmission"] = general_data_only_text[transmission]
+        if "Категория" in general_data_only_text:
+            car_type = general_data_only_text.index("Категория") + 1
+            car_dict["Car_type"] = general_data_only_text[car_type]
+        if "Пробег" in general_data_only_text:
+            kilometers_traveled = general_data_only_text.index("Пробег") + 1
+            car_dict["Kilometers_traveled"] = float(general_data_only_text[kilometers_traveled].split(" ")[0])
+        if "Цвят" in general_data_only_text:
+            colour = general_data_only_text.index("Цвят") + 1
+            car_dict["Colour"] = general_data_only_text[colour]
+        # This gets the price of the car
+        detailed_price_general = html_link.find('span', {'id': 'details_price'}).text
+        # detailed_price = 0
+        if "лв." in detailed_price_general:
+            detailed_price = int(detailed_price_general.replace('лв.', '').replace(" ", ""))
+        else:
+            detailed_price = round(int(detailed_price_general.replace('EUR', '').replace(" ", "")) * EUR_TO_LEV, 0)
+        car_dict["Price"] = detailed_price
+        # Get all additional features
+        additional_features = html_link.find_all('label', {'class': 'extra_cat'})
+        specific_element = []
+        for element in additional_features:
+            for element1 in element.find_next_siblings('div'):
+                specific_element.append(element1.text.replace('•', '').strip())
+        car_dict["Additional_Data"] = specific_element
     except:
         pass
 
     return car_dict
+
 
 all_cars = []
 for page in pages_with_car_offers:
@@ -92,12 +115,13 @@ for page in pages_with_car_offers:
 all_data_list = []
 counter = 0
 for detailed_page in all_cars:
+    random_delay = round(random.uniform(3, 6), 2)
     page_source_detail = get_page_source_with_selenium(detailed_page)
     detailed_dict = get_exact_data_for_each_car(page_source_detail)
     all_data_list.append(detailed_dict)
     counter += 1
     print(counter)
-    time.sleep(5)
+    time.sleep(random_delay)
 
 
 df = pd.DataFrame.from_dict(all_data_list)
@@ -105,4 +129,4 @@ df2 = df.dropna().reset_index(drop=True)
 
 print(df2)
 
-df2.to_excel("test.xlsx")
+df2.to_excel(f"data_{TODAYS_DATE}.xlsx")
